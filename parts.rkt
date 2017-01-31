e81204d75a912cf1f7efc3be95174c3e2f15a49e
Tom Gillespie <tgbugs@gmail.com>
2017-01-30 17:00:46 -0800

    parts provide

--- parts.rkt
+++ parts.rkt
@@ -1,11 +1,14 @@
+#lang racket
+(provide NUMKEYS)
 ; BOM?
-(microcontroller (must "trackpoint" "matrix" "led"))
-(switch mx-series (color UNKNOWN)
-        (compatible-with
-         (led (size (units 3mm)))
-         (led (size (units '(2 3 4) mm)))
-         (led (size (units 1.8 mm)))))
-(led (size (units UNKOWN mm)) (color UNKNOWN)) ; flangeless,
-(housing WHEEE)
+(define NUMKEYS 92)
+(microcontroller 'teensy-3.2 (must "trackpoint" "matrix" "led")
+                 (define PINOUT (outputs (digital 34))))
+(instances NUMKEYS (switch mx-series (color UNKNOWN)
+                         (compatible-with
+                          (led (size (units 3mm)))
+                          (led (size (units '(2 3 4) mm)))
+                          (led (size (units 1.8 mm))))))
+(led (size (units UNKOWN mm)) (color UNKNOWN)) ; flangeless,(housing WHEEE)
 (mounting-plate WHEEE)
 (pcb OHGODNO) ; MAX DIODES
