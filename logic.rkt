ed3ab18b82ec1ff551a8fae97ffcd081710c3a5f
Tom Gillespie <tgbugs@gmail.com>
2017-01-30 17:01:28 -0800

    some math on pin counts, assuming SMD is viable

---
+++ logic.rkt
@@ -0,0 +1,9 @@
+#lang racket
+; logic
+(require "parts.rkt")
+
+; (define PINSTOTAL (digital (outputs teensy-3.2))) -> 34
+; (define PINSMAT (* 2 (ceiling (sqrt NUMKEYS)))) -> 20
+; (defines PINSTP 2) ; 5V comes from VUSB and GND doesn't count to the total
+; (- PINSTOTAL PINSMAT PINSTP) -> 12
+
