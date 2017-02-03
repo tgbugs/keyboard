49ed02858b70354fab58668efd4633d1b924c246
Tom Gillespie <tgbugs@gmail.com>
2017-02-03 14:45:38 -0800

    LVC9 measured params

--- LVC9.rkt
+++ LVC9.rkt
@@ -58,20 +58,22 @@
    (output 7 GND)))
 
 (define DIMS
-  (list
-   (parameter* '(vector TOP LEFT) (units (vector 0 0) 'mm)) ; how to use this to define/get/transform the rest of the positions, this is a given, the rest need to be filled in using this given... but that is an implementation detail?
-   (parameter* PCB '*thickness (units (measure-TODO) 'mm))  ; fun... this needs to be filled in using a measure... some syntax could help here... more fun with the fact that 'thickenss is implicitly about some rectangular prism or something
-   (parameter* PCB '*length (units (measure-TODO) 'mm)) ; l vs w 
-   (parameter* PCB '*width (units (measure-TODO) 'mm))
-   (parameter* JOYPOST '*width (units (measure-TODO) 'mm))
-   (parameter* JOYHEAD '*width (units (measure-TODO) 'mm))
-   (parameter* JOYPOST '*position (units (measure-TODO) 'mm))
-   (parameter* JOYPAD '*thickness (units (measure-TODO) 'mm)) ; matches the chips?
-   (parameter* SH1 '*diameter (units (measure-TODO) 'mm))
-   (parameter* SH1 '*position (units (measure-TODO) 'mm))
-   (parameter* SH2 '*position (units (measure-TODO) 'mm))
-   (parameter* SH3 '*position (units (measure-TODO) 'mm))
-   (parameter* SH4 '*position (units (measure-TODO) 'mm))))
+  (list ; TODO how to document the fact that I used (measure-lenght (ruler fiskars-stainless-steel)) to obtain the parameters... we need a human way to do this without having to force someone to use an external data system
+   (parameter* '(vector LEFT TOP) (units (vector 0 0) 'mm)) ; how to use this to define/get/transform the rest of the positions, this is a given, the rest need to be filled in using this given... but that is an implementation detail?
+   (define (*position units-vector) ((position-from-reference (vector LEFT TOP)) units-vector)) ; better way? how to warn if missing?
+   (parameter* PCB '*thickness (units 1 'mm))  ; fun... this needs to be filled in using a measure... some syntax could help here... more fun with the fact that 'thickenss is implicitly about some rectangular prism or something
+   (parameter* PCB '*length (units 31 'mm)) ; l vs w
+   (parameter* PCB '*width (units 29.5 'mm))
+   (parameter* JOYPOST '*width (units 2.5 'mm)) ; effectively a cube
+   (parameter* JOYHEAD '*width (units 4.1 'mm)) ; may be a bit closer to 4mm but doesn't actually matter since we're not making it
+   (parameter* JOYPOST '*position (units (vector 16 7.5) 'mm))
+   (parameter* JOYPAD '*thickness (units 1 'mm)) ; matches the chips?
+   (parameter* JOYPAD '*diameter (units 7.5 'mm)) ; ⌀8mm for the keyboard hole
+   (parameter* SH1 '*diameter (units 2.5 'mm))
+   (parameter* SH1 '*position (units (vector 6.5 7.5) 'mm)) ; using (parameter* JOYPOST '*position (vector 0 0)) => (vector -9.5 0)
+   (parameter* SH2 '*position (units (vector 26 7.5) 'mm)) ; (vector 9.5 0)
+   (parameter* SH3 '*position (units (vector 15.5 7.5) 'mm)) ; (vector -8.5 8)
+   (parameter* SH4 '*position (units (vector 15.5 25) 'mm)))) ; (vector 8.5 8)
 
 (define-measure (*position thing)
   (vector x y z))
