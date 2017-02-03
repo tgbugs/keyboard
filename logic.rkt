39a481271869aada8fca6ec78f40fae7631c2c88
Tom Gillespie <tgbugs@gmail.com>
2017-02-03 14:46:30 -0800

    logic typo

--- logic.rkt
+++ logic.rkt
@@ -4,6 +4,6 @@
 
 ; (define PINSTOTAL (digital (outputs teensy-3.2))) -> 34
 ; (define PINSMAT (* 2 (ceiling (sqrt NUMKEYS)))) -> 20
-; (defines PINSTP 2) ; 5V comes from VUSB and GND doesn't count to the total
+; (define PINSTP 2) ; 5V comes from VUSB and GND doesn't count to the total
 ; (- PINSTOTAL PINSMAT PINSTP) -> 12
 
