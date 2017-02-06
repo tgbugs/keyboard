8549850d3343fc45e185a62b53c52c511d3a0484
Tom Gillespie <tgbugs@gmail.com>
2017-02-06 01:58:33 -0800

    updated counts to reflect changes in number of keys

--- logic.rkt
+++ logic.rkt
@@ -3,7 +3,8 @@
 (require "parts.rkt")
 
 ; (define PINSTOTAL (digital (outputs teensy-3.2))) -> 34
-; (define PINSMAT (* 2 (ceiling (sqrt NUMKEYS)))) -> 20
+; (define PINSMAT (* 2 (ceiling (sqrt NUMKEYS)))) -> 20  ; with 89 keys 6x15 possible
 ; (define PINSTP 2) ; 5V comes from VUSB and GND doesn't count to the total
-; (- PINSTOTAL PINSMAT PINSTP) -> 12
+; (define PINSLED 6) ; 3 data 3 clock to each of 3 STP16CP05
+; (- PINSTOTAL PINSMAT PINSTP PINSLED) -> 6 ; or 5 in a 6x15
 
